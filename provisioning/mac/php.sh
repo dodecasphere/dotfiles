50cd8f4f770fab889b417f9a86fcd748a2e377a1
Michael Dulle <dodecasphere@users.noreply.github.com>
2021-02-06 00:40:52 -0500

    Update php.sh

--- provisioning/mac/php.sh
+++ provisioning/mac/php.sh
@@ -2,6 +2,7 @@
 
 #
 # Set up PHP, MySQL, Composer and Laravel
+# USE DBNGIN INSTEAD OF INSTALLING DATABASES
 #
 
 # Weird thing with macOS, might need to run `sudo chown -R $(whoami) $(brew --prefix)/*` to fix permissions, per https://stackoverflow.com/questions/16432071/how-to-fix-homebrew-permissions
@@ -9,21 +10,21 @@
 # Also, see this if getting weird issues with authentication: https://laracasts.com/discuss/channels/laravel/caching-sha2-password-error-when-running-php-artisan-migrate
 
 # Start with MySQL
-doing "Installing MySQL..."
-brew install mysql@5.7
+# doing "Installing MySQL..."
+# brew install mysql@5.7
 
-doing "Starting MySQL as a service..."
-brew services start mysql@5.7
+# doing "Starting MySQL as a service..."
+# brew services start mysql@5.7
 
-doing "Setting up MySQL Password..."
-mysql_secure_installation
+# doing "Setting up MySQL Password..."
+# mysql_secure_installation
 
-doing "Creating databases..."
-mysql -u root -e "create database family;" -v
-mysql -u root -e "create database smart-mirror;" -v
-mysql -u root -e "create database crestlite55;" -v
-mysql -u root -e "create database dra;" -v
-mysql -u root -e "create database concrete;" -v
+# doing "Creating databases..."
+# mysql -u root -e "create database family;" -v
+# mysql -u root -e "create database smart-mirror;" -v
+# mysql -u root -e "create database crestlite55;" -v
+# mysql -u root -e "create database dra;" -v
+# mysql -u root -e "create database concrete;" -v
 
 # NOTE THIS: https://flipdazed.github.io/blog/osx%20maintenance/set-up-mysql-osx
 
@@ -60,13 +61,13 @@ sudo mv composer.phar /usr/local/bin/composer
 # Make sure to place the $HOME/.composer/vendor/bin directory (or the equivalent directory for your OS)
 # in your $PATH so the laravel executables can be located by your system.
 doing "Installing Laravel things..."
-composer global require "laravel/installer"
-composer global require "laravel/lumen-installer"
+# composer global require "laravel/installer"
+# composer global require "laravel/lumen-installer"
 composer global require "laravel/valet"
 composer global require "laravel/envoy"
 composer global require "tightenco/collect"
 composer global require "tightenco/lambo"
-composer global require "localheinz/composer-normalize"
+composer global require "ergebnis/composer-normalize"
 composer global require "squizlabs/php_codesniffer"
 composer global require "beyondcode/expose"
 
@@ -84,11 +85,11 @@ brew install mailhog
 doing "Starting Mailhog as a service..."
 brew services start mailhog
 
-doing "Installing Redis..."
-brew install redis
+# doing "Installing Redis..."
+# brew install redis
 
-doing "Starting Redis as a service..."
-brew services start redis
+# doing "Starting Redis as a service..."
+# brew services start redis
 
 doing "Installing Deployer..."
 curl -LO https://deployer.org/deployer.phar
